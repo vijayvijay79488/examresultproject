fbebbda856a4824e55c1fec94f30b351c31202c1
Dell <ramyadevip.0321@gmail.com>
2025-05-12 09:50:53 +0530

    Added Revaluations

--- src/main/java/com/example/examresult/repository/revalutionrepo.java
+++ src/main/java/com/example/examresult/repository/revalutionrepo.java
@@ -1,10 +1,12 @@
 package com.example.examresult.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.examresult.model.revalutionmodel;
 
 public interface revalutionrepo extends JpaRepository<revalutionmodel, Integer> {
-	revalutionmodel findByRegisteredAndSemester(int registered, int semester);
+	List<revalutionmodel> findByStatus(String status);
 
 }
